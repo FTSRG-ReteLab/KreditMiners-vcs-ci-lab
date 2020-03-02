71883879414bc466452c6bb7ce9448a06da2b448
Nyizsnyik Benjamin <nyizsnyik.benjamin@gmail.com>
2020-03-02 16:51:34 +0100

    Adds Guava table

---
+++ train-tachograph/src/main/java/TrainTachograph.java
@@ -0,0 +1,6 @@
+import com.google.common.collect.Table;
+import com.google.common.collect.TreeBasedTable;
+
+public class TrainTachograph {
+    Table<Long, Integer, Integer> table = TreeBasedTable.create();
+}
