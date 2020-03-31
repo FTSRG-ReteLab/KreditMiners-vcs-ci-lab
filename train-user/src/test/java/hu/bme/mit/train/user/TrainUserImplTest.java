6d1203d707ec1774aa6f8f3dadc78177d5d5a46a
Nyizsnyik Benjámin <nyizsnyik.benjamin@gmail.com>
2020-03-31 15:41:10 +0200

    Implement alarmState + extend unit tests + smaller changes

--- train-user/src/test/java/hu/bme/mit/train/user/TrainUserImplTest.java
+++ train-user/src/test/java/hu/bme/mit/train/user/TrainUserImplTest.java
@@ -1,14 +1,27 @@
 package hu.bme.mit.train.user;
 
 
+import hu.bme.mit.train.controller.TrainControllerImpl;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class TrainUserImplTest {
-    TrainUserImpl trainUser = new TrainUserImpl(null);
+    TrainUserImpl trainUser = new TrainUserImpl(new TrainControllerImpl());
 
     @Test
     public void testGetAlarmFlag() {
         Assert.assertFalse(trainUser.getAlarmFlag());
+    }
+
+    @Test
+    public void testGetReferenceSpeed() throws InterruptedException {
+        trainUser.overrideJoystickPosition(5);
+        Thread.sleep(5000);
+
+        trainUser.overrideJoystickPosition(0);
+        Thread.sleep(2000);
+
+        trainUser.overrideJoystickPosition(-2);
+        Thread.sleep(20000);
     }
 }
